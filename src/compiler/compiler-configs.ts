5164e6279ef098b9e362a16e33ec6034eed574c0
J Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2018-07-21 01:25:19 -0300

    removed compiler-configs.ts and compiler-configs-parser.ts

--- src/compiler/compiler-configs.ts
+++
@@ -1,9 +0,0 @@
-export interface ICompilerConfigs {
-    compilerName: string,
-    filePath?: string
-    executionTimeout?: number
-    compileCommand?: string
-    runCommand?: string
-    variables: Map<string, string | number | boolean>
-    inputs: string[]
-}
